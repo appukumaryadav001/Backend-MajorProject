032be9c7d77b6522e6c65196b13153738d1ccc2c
Appukumaryadav001 <appukumaryadav19@gmail.com>
2026-02-20 07:45:06 +0530

    add tweet router

--- src/app.js
+++ src/app.js
@@ -22,6 +22,7 @@ import videoRouter from "./routes/video.routes.js";
 import subscriptionRouter from "./routes/subscription.routes.js";
 import playlistRouter from "./routes/playlist.routes.js";
 import commentRouter from "./routes/comment.routes.js";
+import tweetRouter from "./routes/tweet.routes.js";
 
 
 app.use("/api/v1/users",userRouter);
@@ -29,5 +30,6 @@ app.use("/api/v1/videos",videoRouter);
 app.use("/api/v1/subscriptions",subscriptionRouter);
 app.use("/api/v1/playlists",playlistRouter);
 app.use("/api/v1/comments",commentRouter);
+app.use("/api/v1/tweets",tweetRouter);
 
 export {app};
